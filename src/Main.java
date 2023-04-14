d615a8d830bd09c8156b9b0ca963ddaea90d1621
arogoff <asr8607@rit.edu>
2023-04-14 10:50:18 -0400

    Set up basic structure.
    LoadIngredients created - Job is to return an Array or ArrayList of Ingredient objects from the ingredients.csv file.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+import src.utils.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Main");
+        LoadIngredients.readIn();
+    }
+}
---
+++ src/utils/LoadIngredients.java
@@ -0,0 +1,9 @@
+package src.utils;
+
+public class LoadIngredients {
+    public static String[] readIn(){
+        String[] strings = {"test"};
+        System.out.println("In LoadIngredients");
+        return strings;
+    }
+}
