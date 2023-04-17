48b149a5d62684f18dc7c6093a102d6f7ed71cc4
Eric Cybulski <Seahawks1667@aol.com>
2023-04-17 09:47:10 -0400

    Updated User.java -Added abstract Food class -Added Recipe class -Added Meal class -Added Workout class -Added Goal class

--- src/objs/User.java
+++ src/objs/User.java
@@ -69,5 +69,166 @@ public class User {
 
     public String birthDate;
 
+}
+
+    abstract class Food {
+        int calories;
+        int fat;
+        int protein;
+        int fiber;
+        int carbs;
+
+        public void setCalories(int calories) { this.calories = calories; }
+        public int getCalories() { return calories; }
+        public void setFat(int fat) { this.fat = fat; }
+        public int getFat() { return fat; }
+        public void setProtein(int protein) { this.protein = protein; }
+        public int getProtein() { return protein; }
+        public void setFiber(int fiber) { this.fiber = fiber; }
+        public int getFiber() { return fiber; }
+        public void setCarbs(int carbs) { this.carbs = carbs; }
+        public int getCarbs() { return carbs; }
+
+        public Food(String name, int calories, int fat, int protein, int fiber, int carbs) {
+            this.calories = calories;
+            this.fat = fat;
+            this.protein = protein;
+            this.fiber = fiber;
+            this.carbs = carbs;
+        }
+
+        public abstract void createFood();
+
+        public String getNutritionalInfo() {
+            return calories + "" + fat + "" + protein + "" + fiber + "" + carbs;
+        }
+
+    }
+
+class Recipe {
+    int calories;
+    int fat;
+    int protein;
+    int fiber;
+    int carbs;
+
+    public void setCalories(int calories) { this.calories = calories; }
+    public int getCalories() { return calories; }
+    public void setFat(int fat) { this.fat = fat; }
+    public int getFat() { return fat; }
+    public void setProtein(int protein) { this.protein = protein; }
+    public int getProtein() { return protein; }
+    public void setFiber(int fiber) { this.fiber = fiber; }
+    public int getFiber() { return fiber; }
+    public void setCarbs(int carbs) { this.carbs = carbs; }
+    public int getCarbs() { return carbs; }
+
+    public Recipe(String name, int calories, int fat, int protein, int fiber, int carbs) {
+        this.calories = calories;
+        this.fat = fat;
+        this.protein = protein;
+        this.fiber = fiber;
+        this.carbs = carbs;
+    }
+
+    public void createRecipe() {
+
+    }
+
+    public String getNutritionalInfo() {
+        return calories + "" + fat + "" + protein + "" + fiber + "" + carbs;
+    }
+
+}
+
+class Meal {
+    int calories;
+    int fat;
+    int protein;
+    int fiber;
+    int carbs;
+
+    public void setCalories(int calories) { this.calories = calories; }
+    public int getCalories() { return calories; }
+    public void setFat(int fat) { this.fat = fat; }
+    public int getFat() { return fat; }
+    public void setProtein(int protein) { this.protein = protein; }
+    public int getProtein() { return protein; }
+    public void setFiber(int fiber) { this.fiber = fiber; }
+    public int getFiber() { return fiber; }
+    public void setCarbs(int carbs) { this.carbs = carbs; }
+    public int getCarbs() { return carbs; }
+
+    public Meal(String name, int calories, int fat, int protein, int fiber, int carbs) {
+        this.calories = calories;
+        this.fat = fat;
+        this.protein = protein;
+        this.fiber = fiber;
+        this.carbs = carbs;
+    }
+
+    public void createMeal() {
+
+    }
+
+    public String getNutritionalInfo() {
+        return calories + "" + fat + "" + protein + "" + fiber + "" + carbs;
+    }
+
+}
+
+class Workout {
+    int minutes;
+    int intensity;
+    String time;
+    String date;
+    int caloriesToAdd;
+
+    public void setMinutes(int minutes) { this.minutes = minutes; }
+    public int getMinutes() { return minutes; }
+    public void setIntensity(int intensity) { this.intensity = intensity; }
+    public int getIntensity() { return intensity; }
+    public void setTime(String time) { this.time = time; }
+    public String getTime() { return time; }
+    public void setDate(String date) { this.date = date; }
+    public String getDate() { return date; }
+    public void setCaloriesToAdd(int caloriesToAdd) { this.caloriesToAdd = caloriesToAdd; }
+    public int getCaloriesToAdd() { return caloriesToAdd; }
+
+    public Workout(int minutes, int intensity, String time, String date, int caloriesToAdd) {
+        this.minutes = minutes;
+        this.intensity = intensity;
+        this.time = time;
+        this.date = date;
+        this.caloriesToAdd = caloriesToAdd;
+    }
+
+    public void createWorkout() {
+
+    }
+
+    public String getWorkoutInfo() {
+        return minutes + "" + intensity + "" + time + "" + date + "" + caloriesToAdd;
+    }
+
+}
+
+class Goal {
+    String type;
+
+    public void setType(String type) { this.type = type; }
+    public String getType() { return type; }
+
+    public Goal(String type) {
+        this.type = type;
+    }
+
+    public void createGoal() {
+
+    }
+
+    public String getGoalInfo() {
+        return type;
+    }
 
 }
