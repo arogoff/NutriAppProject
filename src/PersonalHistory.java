bd4b29e0f40b78597cf117ffdddaa8fb3bae32fb
aze6809 <aze6809@rit.edu>
2023-04-24 11:43:14 -0400

    added comments

--- src/PersonalHistory.java
+++ src/PersonalHistory.java
@@ -48,7 +48,7 @@ public class PersonalHistory {
 
          //Total calories gained from all meals stored
          caloriesConsumed += calories;
-       //  System.out.println(test);
+     
      }
      public PersonalHistory(User anon){
     //Will be useful for future use when User things are fully functional
@@ -63,7 +63,7 @@ public class PersonalHistory {
      public void recordHistory(User anon){
         bigString.concat("Weight: " + anon.getWeight() + "\n");
         bigString.concat("Daily target: " + anon.getDailyTarget() + "\n");
-        bigString.concat("You've consumed " + caloriesConsumed + " calories. \n");
+        bigString.concat("You've consumed " + caloriesConsumed + " calories.\n");
 
         for (String thing : meals) {
             bigString.concat(thing + "\n");
@@ -72,50 +72,20 @@ public class PersonalHistory {
         for (String exercise : workouts) {
             bigString.concat(exercise + "\n");
         }
-
+        try {
+            FileWriter fw = new FileWriter("src/databases/histories.csv", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(anon.getUsername() + ", " + anon.getDailyTarget() + ", " + bigString);
+            bw.close();
+            //System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("Error [UserUtils.createUser] ! An error occurred: ");
+            e.printStackTrace();
+        }
      }
-     //Test method to see if PersonalHistory functions as intended.
-     public static void test(){
-         User nobody = new User("dummy", "OOgswgvRGIaeBR=", "tommy",
-          "danger", "3000", "5ft 8","160", "11/17/2002", "m", "notset");
-          
-          TrackingWorkout tracker = new TrackingWorkout(1000, 5, 7, 10);
-          tracker.recordWorkout(30, "medium");
-          tracker.recordWorkout(10, "high");
-
-          System.out.println("You've burned " + caloriesLost + " calories.");
-          System.out.println("You've consumed " + caloriesConsumed + " calories.");
-         // System.out.println("Total calories burned: " + tracker.getCaloriesBurned());
-          System.out.println("Remaining calories to meet daily target: " + (Integer.parseInt(nobody.getDailyTarget()) - caloriesLost));
-          
-          TrackingMeal mealTracker = new TrackingMeal(2000);
-         
-
-        /**
-         * code for writing all saved info into .scsv file
-                try {
-                    FileWriter fw = new FileWriter("src/databases/histories.csv", true);
-                    BufferedWriter bw = new BufferedWriter(fw);
-                    bw.write(username + "," + dailyTarget + "," + meals +"," + workouts + "\n");
-                    bw.close();
-                    //System.out.println("Successfully wrote to the file.");
-                } catch (IOException e) {
-                    System.out.println("Error [UserUtils.createUser] ! An error occurred: ");
-                    e.printStackTrace();
-                }
-        */
- 
- 
-         System.out.println("Weight: " + nobody.getWeight());
- 
-         System.out.println("Daily target: " + nobody.getDailyTarget());
-        
-
- 
-     }
+     
  
      public static void main(String[] args) {
-        // System.out.println("In personal history...");
-         test();
+       
      }
 }
